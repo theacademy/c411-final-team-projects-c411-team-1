2e309c14ddaf782b9071e9e51b6249e3fc349dc3
YaraJan <yajandal@gmail.com>
2025-03-25 15:41:56 -0500

    Fixed bug in StockPortfolioService

--- ETrade/src/main/java/com/mthree/etrade/service/StockPortfolioService.java
+++ ETrade/src/main/java/com/mthree/etrade/service/StockPortfolioService.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 public interface StockPortfolioService {
     StockPortfolio addStockToPortfolio(StockPortfolio stockPortfolio);
+
     StockPortfolio updateStockInPortfolio(Long id, StockPortfolio stockPortfolio);
+
     void removeStockFromPortfolio(Long id);
+
     List<StockPortfolio> getPortfolioByUserId(Long userId);
+
     double calculatePortfolioValue(Long userId);
+}
