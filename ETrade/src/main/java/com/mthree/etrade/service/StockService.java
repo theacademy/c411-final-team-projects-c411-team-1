f9279d6e57147cd01b8ddee6f4245d090f85a222
YaraJan <yajandal@gmail.com>
2025-03-25 10:32:19 -0500

    Completed StockService Interface

--- ETrade/src/main/java/com/mthree/etrade/service/StockService.java
+++ ETrade/src/main/java/com/mthree/etrade/service/StockService.java
@@ -1,4 +1,20 @@
 package com.mthree.etrade.service;
 
+import com.mthree.etrade.model.Stock;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 public interface StockService {
+
+    Stock getStock(String symbol);
+
+    List<Stock> getAllStocks();
+
+    BigDecimal getCurrentPrice(String symbol);
+
+    Stock updateStock(String symbol, Stock stock);
+
+    List<BigDecimal> getStockHistory(String symbol, Date startDate, Date endDate);
 }
