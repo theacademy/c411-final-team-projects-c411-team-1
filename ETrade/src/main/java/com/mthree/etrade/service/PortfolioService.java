c76984e41f345dc63f0601a9b6f564b07c9170f8
jacquelinesaad <jacquelinesaad1@aol.com>
2025-03-25 16:04:39 -0400

    Update PortfolioService with calculateTotalValue method

--- ETrade/src/main/java/com/mthree/etrade/service/PortfolioService.java
+++ ETrade/src/main/java/com/mthree/etrade/service/PortfolioService.java
@@ -1,4 +1,19 @@
 package com.mthree.etrade.service;
 
+import com.mthree.etrade.model.Portfolio;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface PortfolioService {
+
+    Portfolio savePortfolio(Portfolio portfolio);
+    Portfolio getPortfolioById(Long id);
+    List<Portfolio> getAllPortfolios();
+    List<Portfolio> getPortfoliosByUserId(Long userId);
+    void deletePortfolioById(Long id);
+
+    BigDecimal calculateTotalValue(Long portfolioId);
+
+    // TODO: implement performanceMetrics(int portfolioId)
 }
