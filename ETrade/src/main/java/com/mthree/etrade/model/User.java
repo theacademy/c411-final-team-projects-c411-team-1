5d562825d32b784d322e66af719f0e36d63bb378
AymanMansoor <aimanmhussain@outlook.com>
2025-03-26 12:13:43 -0400

    second User update

--- ETrade/src/main/java/com/mthree/etrade/model/User.java
+++ ETrade/src/main/java/com/mthree/etrade/model/User.java
@@ -1,93 +1,87 @@
 package com.mthree.etrade.model;
 
-public class User {
-    package com.mthree.etrade.model;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Objects;
 
-    @Entity
-    @Table(name = "users")
-    public class User {
+@Entity
+@Table(name = "users")
+public class User {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private int id;
+    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-        @Column(nullable = false, length = 50)
-        private String name;
+    @Column(nullable = false, length = 50)
+    private String name;
 
-        @Column(nullable = false, unique = true, length = 100)
-        private String email;
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
 
-        @Column(nullable = false, length = 255)
-        private String password;
+    @Column(nullable = false, length = 255)
+    private String password;
 
-        @Column(nullable = false, precision = 15, scale = 2)
-        private BigDecimal balance;
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal balance;
 
-        // Constructors
-        public User() {}
-
-        public User(String name, String email, String password, BigDecimal balance) {
-            this.name = name;
-            this.email = email;
-            this.password = password;
-            this.balance = balance;
-        }
-
-        // Getters
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public BigDecimal getBalance() {
-            return balance;
-        }
-
-        // Setters
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public void setBalance(BigDecimal balance) {
-            this.balance = balance;
-        }
-
-        // String representation for debugging/logging
-        @Override
-        public String toString() {
-            return "User{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", email='" + email + '\'' +
-                    ", balance=" + balance +
-                    '}';
-        }
+    public int getId() {
+        return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(balance, user.balance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, balance);
+    }
 }
