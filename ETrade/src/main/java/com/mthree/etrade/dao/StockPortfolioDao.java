85dd3fe42a9c74c11c2ca6e55522a0c930993a1a
fzmehrizi <65924618+fzmehrizi@users.noreply.github.com>
2025-03-26 16:50:53 -0400

    Update StockPortfolioDao.java

--- ETrade/src/main/java/com/mthree/etrade/dao/StockPortfolioDao.java
+++ ETrade/src/main/java/com/mthree/etrade/dao/StockPortfolioDao.java
@@ -1,6 +1,7 @@
 package com.mthree.etrade.dao;
 
 import com.mthree.etrade.model.StockPortfolio;
+import com.mthree.etrade.model.StockPortfolioId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface StockPortfolioDao extends JpaRepository<StockPortfolio, Long> {
+public interface StockPortfolioDao extends JpaRepository<StockPortfolio, StockPortfolioId> {
 
     // Find all stock entries for a specific portfolio
     List<StockPortfolio> findByPortfolio_PortfolioId(Long portfolioId);
@@ -17,7 +18,6 @@ public interface StockPortfolioDao extends JpaRepository<StockPortfolio, Long> {
     Optional<StockPortfolio> findByPortfolio_PortfolioIdAndStock_Symbol(Long portfolioId, String symbol);
 
     // Delete a specific stock from a portfolio
-    // Added this method that was referenced in TransactionServiceImpl
     void deleteByPortfolio_PortfolioIdAndStock_Symbol(Long portfolioId, String symbol);
 
     // Optional: Get all entries by stock
