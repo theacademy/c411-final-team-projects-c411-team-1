8434cd5d787cbd536b93fceb7041e0f66fc3e8ac
AymanMansoor <79471972+AymanMansoor@users.noreply.github.com>
2025-03-26 14:53:06 -0400

    Started UserController

--- ETrade/src/main/java/com/mthree/etrade/controller/UserController.java
+++ ETrade/src/main/java/com/mthree/etrade/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.mthree.etrade.controller;
 
+import com.mthree.etrade.model.User;
+import com.mthree.etrade.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+    
 }
