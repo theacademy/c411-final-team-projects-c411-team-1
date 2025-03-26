9c184d94c133751443f32c4d0cc4b1bfcf390a1b
fzmehrizi <65924618+fzmehrizi@users.noreply.github.com>
2025-03-25 20:50:28 -0400

    Updated StockPortfolioService Interface

--- ETrade/src/main/java/com/mthree/etrade/service/StockPortfolioService.java
+++ ETrade/src/main/java/com/mthree/etrade/service/StockPortfolioService.java
@@ -6,12 +6,17 @@ import java.util.List;
 
 public interface StockPortfolioService {
     StockPortfolio addStockToPortfolio(StockPortfolio stockPortfolio);
-
     StockPortfolio updateStockInPortfolio(Long id, StockPortfolio stockPortfolio);
-
     void removeStockFromPortfolio(Long id);
 
+    StockPortfolio getStockPortfolioById(Long id);
+    List<StockPortfolio> getStocksByPortfolioId(Long portfolioId);
     List<StockPortfolio> getPortfolioByUserId(Long userId);
+    //Get All Records (useful for admin)
+    List<StockPortfolio> getAllStockPortfolios();
 
     double calculatePortfolioValue(Long userId);
+    //Method for checking existence (to prevent duplicate symbols in a portfolio):
+    boolean existsByPortfolioAndStock(Long portfolioId, String stockSymbol);
 }
+
