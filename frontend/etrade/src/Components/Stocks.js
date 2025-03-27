ad8918a208f4e1c1b6325163cfd5fb7a0fc46a68
YaraJan <yajandal@gmail.com>
2025-03-27 16:24:55 -0500

    Unfinished Stock page frontend

--- frontend/etrade/src/Components/Stocks.js
+++ frontend/etrade/src/Components/Stocks.js
@@ -7,18 +7,49 @@ function Stocks() {
     const [stockName, setStockName] = useState("");
     const [stockCompany, setStockCompany] = useState("");
 
-    const handleGetStocks = async() => {
+    //handle when input is changing (allows user to type in input)
+    const stockNameInput = event => {
+        setStockName(event.target.value);
+    }
+    const stockCompanyInput = event => {
+        setStockCompany(event.target.value);
+    }
+
+    //handle the button click events
+    const handleRefresh = () => {
+        setShowList(!showList)
+    }
+
+    const handleGetStocks = async() => {    //calls getStock api from backend
         try {
             const data = await (await fetch("http://localhost:8080/api/stocks")).json()
             setStockList(data)
-            setShowList(!showList)
         } catch(err) {
             console.log(err.message)
         }
     }
 
-    const stockNameInput = event => {   //handle when input is changing (allows user to type in input)
-        setStockName(event.target.value);
+    const handleAddStock = async() => {    //calls createStock api from backend
+        try {
+            const response = await fetch('http://localhost:8080/api/stocks', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "symbol": "aaple",
+                    "companyName": "Apple Inc"
+                })
+              })
+
+            if(!response.created) {
+                console.log(response.status)
+                throw new Error("Error creating object ${response.status}");
+            }
+
+        } catch(err) {
+            console.log(err.message)
+        }
     }
 
     const handleConsole = () => {
@@ -32,12 +63,17 @@ function Stocks() {
             <div>
                 Test
             </div>
-            <button className="" onClick={handleGetStocks}>
-                Get All Stocks
-            </button>
-            {/* <button className='' onClick={handleConsole}>
+            <div className='flex flex-row justify-center gap-4'>
+                <button className="" onClick={handleGetStocks}>
+                    Get All Stocks
+                </button>
+                <button className='' onClick={handleRefresh}>
+                    Refresh
+                </button>
+            </div>
+            <button className='' onClick={handleConsole}>
                 get console
-            </button> */}
+            </button>
             <div className='flex flex-row justify-center'>
                 <label className='flex mr-4'>Enter stock symbol</label>
                 <input
@@ -49,11 +85,11 @@ function Stocks() {
                 <label className='flex mr-4'>Enter company name</label>
                 <input
                     className='mr-4 border'
-                    placeholder='Enter stock name here'
-                    value={stockName}
-                    onChange={stockNameInput}
+                    placeholder='Enter company name here'
+                    value={stockCompany}
+                    onChange={stockCompanyInput}
                 />
-                <button className='flex'>
+                <button className='flex' onClick={handleAddStock}>
                     add Stock
                 </button>
             </div>
@@ -62,10 +98,10 @@ function Stocks() {
             </div>
             <div>
                 { showList && (
-                    <div className='flex flex-row'>
+                    <div className='flex flex-col'>
                         {stockList.map((stock) => (
                                 <div>
-                                    {stock.symbol} {stock.companyName}
+                                    {stock.companyName}: {stock.symbol}
                                 </div>
                             ))}
                     </div>
