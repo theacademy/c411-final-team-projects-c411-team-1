aeddd2372db8c80c970e932577dbad6efebfb09e
AymanMansoor <79471972+AymanMansoor@users.noreply.github.com>
2025-03-27 14:17:05 -0400

    UserDaoTest still giving errors

--- ETrade/src/main/java/com/mthree/etrade/dao/UserDao.java
+++ ETrade/src/main/java/com/mthree/etrade/dao/UserDao.java
@@ -4,7 +4,9 @@ import com.mthree.etrade.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
-    User findByEmail(String email);  // Find user by email
-}
+    User findByEmail(String email);
+}  // Find user by email
